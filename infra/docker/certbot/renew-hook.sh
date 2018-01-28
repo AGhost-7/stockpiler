c5edef88c57903bc1fa1911c44e59c064aa707ce
AGhost-7 <jonathan.boudreau.92@gmail.com>
2018-01-28 12:31:19 -0500

    Use public image instead

--- infra/docker/certbot/renew-hook.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-for livedir in /etc/letsencrypt/live/*; do
-	domain="$(basename "$livedir")"
-	cat "$livedir"/{fullchain.pem,privkey.pem} > /etc/haproxy/certs/"$domain".pem
-done
